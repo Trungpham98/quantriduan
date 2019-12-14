d34df7888304804d09ad15bc4fe487f1bf3895d6
= <=>
2019-12-14 09:35:23 +0700

    Trung-commit5

---
+++ sources/src/components/SideLeft.vue
@@ -0,0 +1,93 @@
+<template>
+  <v-navigation-drawer
+    fixed
+    :clipped="$vuetify.breakpoint.mdAndUp"
+    app
+    v-model="value.drawer"
+  >
+    <v-list dense>
+      <template v-for="item in items">
+        <v-layout
+          row
+          v-if="item.heading"
+          align-center
+          :key="item.heading"
+        >
+          <v-flex xs6>
+            <v-subheader v-if="item.heading">
+              {{ item.heading }}
+            </v-subheader>
+          </v-flex>
+          <v-flex xs6 class="text-xs-center">
+            <a href="#!" class="body-2 black--text">EDIT</a>
+          </v-flex>
+        </v-layout>
+        <v-list-group
+          v-else-if="item.children"
+          v-model="item.model"
+          :key="item.text"
+          :prepend-icon="item.model ? item.icon : item['icon-alt']"
+          append-icon=""
+        >
+          <v-list-tile slot="activator">
+            <v-list-tile-content>
+              <v-list-tile-title>
+                {{ item.text }}
+              </v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile
+            v-for="(child, i) in item.children"
+            :key="i">
+            <v-list-tile-action class="info-icon" v-if="child.icon">
+              <v-icon>{{ child.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>
+                {{ child.text }}
+              </v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list-group>
+        <v-list-tile v-else :to="item.path" :key="item.text">
+          <v-list-tile-action class="info-icon">
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              {{ item.text }}
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </template>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+<script>
+export default {
+  props:{
+    items: {
+      type: Array
+    },
+    value: {
+      type: Object
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.v-list {
+  .v-list__tile {
+    .v-list__tile__title {
+      font-size: 16px;
+    }
+  }
+
+  .v-list__tile:hover {
+    .v-list__tile__title {
+      color: #2196f3;
+    }
+  }
+}
+</style>
---
+++ sources/src/components/TopBar.vue
@@ -0,0 +1,42 @@
+<template>
+<v-toolbar color="blue darken-3" dark app :clipped-left="$vuetify.breakpoint.mdAndUp" fixed>
+    <v-toolbar-title style="width: 300px" class="text-xs-left">
+        <v-toolbar-side-icon @click.stop="value.drawer = !value.drawer"></v-toolbar-side-icon>
+        <span class="hidden-sm-and-down">BIZFLY-ADS</span>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn icon large @click="$router.push({path: '/profile'})">
+        <v-avatar size="34px">
+            <img :src="basicImg" alt="Vuetify">
+        </v-avatar>
+    </v-btn>
+    <v-btn icon title="Logout" @click="handleLogOut">
+        <v-icon>exit_to_app</v-icon>
+    </v-btn>
+</v-toolbar>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            basicImg: 'https://cdn.itviec.com/employers/vccorp/logo/social/pY59TrN7PQff72x8HnEjkUdN/vccorp-logo.PNG'
+        }
+    },
+
+    props: {
+        value: {
+            type: Object
+        }
+    },
+    methods: {
+        handleLogOut() {
+            window.location.href = `${process.env.VUE_APP_AUTH_URL}/logout?service=${process.env.VUE_APP_DASHBOARD_URL}`
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
